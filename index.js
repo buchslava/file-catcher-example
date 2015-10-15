45e8eeeeaa5931e4863edc99852b0f008f7609eb
buchslava <vyacheslav.chub@gmail.com>
2015-10-15 14:36:22 +0300

    Updated: ++cors settings

--- index.js
+++ index.js
@@ -8,18 +8,22 @@ var streams = require('memory-streams');
 var app = express();
 
 var whitelist = ['http://valor-software.github.io/ng2-file-upload/', 'http://localhost:8080'];
-var corsOptions = {
-  origin: function(origin, callback){
-    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
-    callback(null, originIsWhitelisted);
+
+var corsOptionsDelegate = function(req, callback){
+  var corsOptions;
+  if(whitelist.indexOf(req.header('Origin')) !== -1){
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  }else{
+    corsOptions = { origin: false }; // disable CORS for this request
   }
+  callback(null, corsOptions); // callback expects two parameters: error and options
 };
 
 app.get('/', function (req, res) {
   res.end('Ok!');
 });
 
-app.post('/api', cors(corsOptions), function (req, res) {
+app.post('/api', cors(corsOptionsDelegate), function (req, res) {
   var fstream;
   var files = [];
   var busboy = new Busboy({headers: req.headers});
