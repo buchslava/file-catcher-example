f7ca24bb619ebbd5c70b43282d97d20b256b83c3
buchslava <vyacheslav.chub@gmail.com>
2015-10-15 15:01:36 +0300

    Updated: mulii origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://valor-software.github.io, http://valor-software.github.io/ng2-file-upload');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
