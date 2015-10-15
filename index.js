7dbb8b46115f3e59bc1aa1405f40255f0c3014ab
buchslava <vyacheslav.chub@gmail.com>
2015-10-15 13:59:58 +0300

    Updated: get method was added

--- index.js
+++ index.js
@@ -6,25 +6,29 @@ var streams = require('memory-streams');
 
 var app = express();
 
-app.post('/api', function(req, res) {
-    var fstream;
-    var files = [];
-    var busboy = new Busboy({headers: req.headers});
-    busboy.on('file', function (fieldname, file, filename) {
-        // fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
-        // stub
-        fstream = new streams.WritableStream();
-        file.pipe(fstream);
-        fstream.on('close', function() {
-            console.log('file ' + filename + ' uploaded');
-            files.push(filename);
-        });
-    });
+app.get('/api', function (req, res) {
+  res.end('ok!');
+});
 
-    busboy.on('finish', function(){
-        res.redirect('back');
+app.post('/api', function (req, res) {
+  var fstream;
+  var files = [];
+  var busboy = new Busboy({headers: req.headers});
+  busboy.on('file', function (fieldname, file, filename) {
+    // fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+    // stub
+    fstream = new streams.WritableStream();
+    file.pipe(fstream);
+    fstream.on('close', function () {
+      console.log('file ' + filename + ' uploaded');
+      files.push(filename);
     });
-    req.pipe(busboy);
+  });
+
+  busboy.on('finish', function () {
+    res.redirect('back');
+  });
+  req.pipe(busboy);
 });
 
 app.listen(3000);
