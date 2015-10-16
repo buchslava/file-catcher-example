da8098014b3a22261ff1529ff6ddec2856f32b26
buchslava <vyacheslav.chub@gmail.com>
2015-10-16 11:26:42 +0300

    Updated: resume file stream

--- index.js
+++ index.js
@@ -7,21 +7,10 @@ var streams = require('memory-streams');
 var app = express();
 
 app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://valor-software.github.io');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
     next();
 });
 
@@ -31,17 +20,17 @@ app.post('/api', function (req, res) {
   var busboy = new Busboy({headers: req.headers});
   busboy.on('file', function (fieldname, file, filename) {
     // fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
-    // stub
+    // memory stub
     fstream = new streams.WritableStream();
     file.pipe(fstream);
     fstream.on('close', function () {
       console.log('file ' + filename + ' uploaded');
       files.push(filename);
+      file.resume();
     });
   });
 
   busboy.on('finish', function () {
-    // res.redirect('back');
     res.end('ok');
   });
   req.pipe(busboy);
